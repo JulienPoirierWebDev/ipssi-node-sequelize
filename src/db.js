8a32a2ddbb1de6dd8609cb1a70d219c4549ebd29
jupo <jupo@debian.jupowebdev>
2026-03-17 14:16:27 +0100

    update db

--- src/db.js
+++ src/db.js
@@ -1,25 +1,33 @@
 const { Sequelize } = require('sequelize');
 
-const DB_url = process.env.ENV === "DEV" ? process.env.DB_DEV : process.env.DB_PROD
+const isDev = process.env.ENV === 'DEV';
 
-const sequelize = new Sequelize(DB_url)
+const DB_url = isDev ? process.env.DB_DEV : process.env.DB_PROD;
 
+const dialect = isDev ? 'mysql' : 'postgres';
 
-
+const sequelize = new Sequelize(DB_url, {
+  dialect,
+  protocol: dialect,
+  dialectOptions:
+    dialect === 'postgres'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+});
 async function connectionToDB() {
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
 
-  await sequelize.sync();
-
-
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+    await sequelize.sync();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 
-
-
-
-module.exports = {connectionToDB, sequelize};
+module.exports = { connectionToDB, sequelize };
